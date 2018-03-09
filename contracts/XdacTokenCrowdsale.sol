b8e231b8b33c81b52cb98c784f78109eb0d96f68
dmitrst <dmitr.st@gmail.com>
2018-03-09 18:26:26 +0200

    add refunds and whitelist

--- contracts/XdacTokenCrowdsale.sol
+++ contracts/XdacTokenCrowdsale.sol
@@ -1,7 +1,8 @@
 pragma solidity ^0.4.13;
 
 import "../node_modules/zeppelin-solidity/contracts/math/SafeMath.sol";
-import "../node_modules/zeppelin-solidity/contracts/crowdsale/validation/CappedCrowdsale.sol";
+import "../node_modules/zeppelin-solidity/contracts/ownership/Ownable.sol";
+import "../node_modules/zeppelin-solidity/contracts/crowdsale/Crowdsale.sol";
 import "./XdacToken.sol";
 /**
  * @title XdacTokenCrowdsale
@@ -9,15 +10,29 @@ import "./XdacToken.sol";
   * _goal - 1400 ether soft cap
  * _cap - 35400 ether hard cap
  */
-contract XdacTokenCrowdsale is CappedCrowdsale {
+contract XdacTokenCrowdsale is Crowdsale, Ownable {
 
     using SafeMath for uint256;
     uint256[] roundGoals;
     uint256[] roundRates;
     uint256 minContribution;
-    mapping(address => uint256) public balances;
+
+    mapping(address => Contributor) public contributors;
+    //Array of the addresses who participated
+    address[] public addresses;
+    // Amount of wei raised
+    uint256 public weiDelivered;
+
     event TokenWithdraw(address indexed purchaser, uint256 amount);
-    event TokenCalculate(uint curRound, uint256 weiRaised, uint256 weiAmount, uint256 calculatedTokenAmount);
+    event TokenCalculate(uint curRound, uint256 weiDelivered, uint256 weiAmount, uint256 calculatedTokenAmount);
+    event Test(bool x);
+
+    struct Contributor {
+        uint256 eth;
+        bool whitelisted;
+        bool created;
+    }
+
 
     function XdacTokenCrowdsale(
         address _wallet,
@@ -26,7 +41,6 @@ contract XdacTokenCrowdsale is CappedCrowdsale {
         uint256 _minContribution
     ) public
     Crowdsale(_roundRates[0], _wallet, new XdacToken())
-    CappedCrowdsale(_roundGoals[4])
     {
         require(_roundRates.length == 5);
         require(_roundGoals.length == 5);
@@ -35,59 +49,59 @@ contract XdacTokenCrowdsale is CappedCrowdsale {
         minContribution = _minContribution;
     }
 
-
     function getCurrentRate() public view returns (uint256) {
         return roundRates[getCurrentRound()];
     }
 
     function getCurrentRound() public view returns (uint) {
         for (uint i = 0; i < 5; i++) {
-            if(weiRaised < roundGoals[i]) {
+            if (weiDelivered < roundGoals[i]) {
                 return i;
             }
         }
-
-    }
-
-    function getWeiRaised() public view returns (uint256) {
-        return weiRaised;
     }
 
     function getToken() public view returns (ERC20) {
         return token;
     }
 
+    function getContributorValues(address _contributor) view returns (uint256, bool) {
+        Contributor memory contributor = contributors[_contributor];
+        bool is_whitelisted = contributor.whitelisted;
+        return (contributor.eth, is_whitelisted);
+    }
+
+    function whitelistAddresses(address[] _contributors) onlyOwner {
+        for (uint256 i = 0; i < _contributors.length; i++) {
+            _whitelistAddress(_contributors[i]);
+        }
+    }
+
+    function whitelistAddress(address _contributor) onlyOwner {
+        _whitelistAddress(_contributor);
+    }
+
     /**
-     * @dev Withdraw tokens only after crowdsale ends.
+     * @dev Withdraw tokens.
      */
     function withdrawTokens() public {
-        require(capReached());
-        uint256 amount = balances[msg.sender];
-        require(amount > 0);
-        balances[msg.sender] = 0;
-        TokenWithdraw(msg.sender, amount);
-        _deliverTokens(msg.sender, amount);
+        _deliverTokens(msg.sender);
     }
 
     /**
-    * Gets the balance of the specified address.
-    * @param _owner The address to query the the balance of.
-    * @return An uint256 representing the amount owned by the passed address.
-    */
-    function balanceOf(address _owner) public view returns (uint256 balance) {
-        return balances[_owner];
-    }
-
-    /**
-     * Minimum Contribution amount per Contributor is 0.1 ETH.
-     *
-     * @param _beneficiary Address performing the token purchase
-     * @param _weiAmount Value in wei involved in the purchase
+     * @dev Refound tokens. For contributors
      */
-    function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal {
-        require(_weiAmount > minContribution);
-        super._preValidatePurchase(_beneficiary, _weiAmount);
+    function refundTokens() public {
+        _refundTokens(msg.sender);
     }
+
+    /**
+     * @dev Refound tokens. For owner
+     */
+    function refundTokensForAddress(address _contributor) public onlyOwner {
+        _refundTokens(_contributor);
+    }
+
 
     /**
      * @dev the way in which ether is converted to tokens.
@@ -98,11 +112,11 @@ contract XdacTokenCrowdsale is CappedCrowdsale {
         uint curRound = getCurrentRound();
         uint256 calculatedTokenAmount = 0;
         uint256 roundWei = 0;
-        uint256 weiRaisedIntermediate = weiRaised;
+        uint256 weiRaisedIntermediate = weiDelivered;
         uint256 weiAmount = _weiAmount;
 
         for (curRound; curRound < 5; curRound++) {
-            if(weiRaisedIntermediate.add(weiAmount) > roundGoals[curRound]) {
+            if (weiRaisedIntermediate.add(weiAmount) > roundGoals[curRound]) {
                 roundWei = roundGoals[curRound].sub(weiRaisedIntermediate);
                 weiRaisedIntermediate = weiRaisedIntermediate.add(roundWei);
                 weiAmount = weiAmount.sub(roundWei);
@@ -118,7 +132,65 @@ contract XdacTokenCrowdsale is CappedCrowdsale {
         return calculatedTokenAmount;
     }
 
-    function _processPurchase(address _beneficiary, uint256 _tokenAmount) internal {
-        balances[_beneficiary] = balances[_beneficiary].add(_tokenAmount);
+
+    /**
+     * Minimum Contribution amount per Contributor is 0.1 ETH.
+     *
+     * @param _contributor Address performing the token purchase
+     * @param _weiAmount Value in wei involved in the purchase
+     */
+    function _preValidatePurchase(address _contributor, uint256 _weiAmount) internal {
+        require(_weiAmount > minContribution);
+        require(weiDelivered.add(_weiAmount) <= roundGoals[4]);
+        super._preValidatePurchase(_contributor, _weiAmount);
+    }
+
+
+    function _forwardFunds() internal {
+        Contributor memory contributor = contributors[msg.sender];
+        if (contributor.whitelisted) {
+            _deliverTokens(msg.sender);
+        }
+    }
+
+    function _processPurchase(address _contributor, uint256 _tokenAmount) internal {
+        Contributor storage contributor = contributors[_contributor];
+        contributor.eth = contributor.eth.add(msg.value);
+    }
+
+    function _deliverTokens(address _contributor) internal {
+        Contributor storage contributor = contributors[_contributor];
+        uint256 amountEth = contributor.eth;
+        uint256 amountToken = _getTokenAmount(amountEth);
+        require(amountToken > 0);
+        require(amountEth > 0);
+        require(contributor.whitelisted);
+        contributor.eth = 0;
+        weiDelivered = weiDelivered.add(amountEth);
+        wallet.transfer(amountEth);
+        super._deliverTokens(_contributor, amountToken);
+    }
+
+    function _refundTokens(address _contributor) internal {
+        Contributor storage contributor = contributors[_contributor];
+        uint256 ethAmount = contributor.eth;
+        require(ethAmount > 0);
+        contributor.eth = 0;
+        _contributor.transfer(ethAmount);
+        weiRaised = weiRaised.sub(ethAmount);
+        weiDelivered = weiDelivered.sub(ethAmount);
+    }
+
+    function _whitelistAddress(address _contributor) internal {
+        Contributor storage contributor = contributors[_contributor];
+        contributor.whitelisted = true;
+        if (contributor.created == false) {
+            contributor.created = true;
+            addresses.push(_contributor);
+        }
+        //Auto deliver tokens
+        if (contributor.eth > 0) {
+            _deliverTokens(_contributor);
+        }
     }
 }
--- test/XdacTokenCrowdsaleTest.js
+++ test/XdacTokenCrowdsaleTest.js
@@ -48,106 +48,135 @@ contract('XdacTokenCrowdsale', function ([owner, wallet, investor]) {
     this.crowdsale.should.exist
     const rate = await this.crowdsale.rate()
     const walletAddress = await this.crowdsale.wallet()
-    const cap = await this.crowdsale.cap()
     rate.should.be.bignumber.equal(rates[0])
     walletAddress.should.be.equal(wallet)
-    cap.should.be.bignumber.equal(goals[4])
+  })
+  it('should allow whitelist investor', async function () {
+    await this.crowdsale.whitelistAddress(investor).should.be.fulfilled
+    let contributor = await this.crowdsale.getContributorValues(investor).should.be.fulfilled
+    contributor[1].should.be.true
   })
 
-  it('should not accept less then '+minContribution+' ether', async function () {
-    await this.crowdsale.send(ether(minContribution-minContribution/2)).should.be.rejectedWith(EVMRevert)
-    await this.crowdsale.buyTokens(investor, { from: investor, value: ether(minContribution-minContribution/2) }).should.be.rejectedWith(EVMRevert)
+  it('should not accept less then ' + minContribution + ' ether', async function () {
+    await this.crowdsale.whitelistAddress(investor).should.be.fulfilled
+    await this.crowdsale.buyTokens(investor, {
+      from: investor,
+      value: ether(minContribution - minContribution / 2)
+    }).should.be.rejectedWith(EVMRevert)
   })
 
   it('should reject payments over cap', async function () {
-    await this.crowdsale.send(goals[4])
+    await this.crowdsale.whitelistAddress(investor).should.be.fulfilled
+    await this.crowdsale.buyTokens(investor, { from: investor, value: goals[4] }).should.be.fulfilled
     await this.crowdsale.send(1).should.be.rejectedWith(EVMRevert)
   })
 
-  it('check round pre ICO rate', async function () {
-    await this.crowdsale.send(goals[0].minus(ether(0.01)));
-    let rate = await this.crowdsale.getCurrentRate()
-    rate.should.be.bignumber.equal(rates[0])
-  })
+  it('should accept payments during the sale for whitelisted address', async function () {
+    await this.crowdsale.whitelistAddress(investor).should.be.fulfilled
+    let contributor = await this.crowdsale.getContributorValues(investor).should.be.fulfilled
+    contributor[1].should.be.true
 
-  it('check round 1 rate', async function () {
-    await this.crowdsale.send(goals[0].plus(ether(0.01)));
-    let rate = await this.crowdsale.getCurrentRate()
-    rate.should.be.bignumber.equal(rates[1])
-  })
-  it('check round 2 rate', async function () {
-    await this.crowdsale.send(goals[1].plus(ether(0.01)))
-    let rate = await this.crowdsale.getCurrentRate()
-    rate.should.be.bignumber.equal(rates[2])
-  })
-  it('check round 3 rate', async function () {
-    await this.crowdsale.send(goals[2].plus(ether(0.01)))
-    let rate = await this.crowdsale.getCurrentRate()
-    rate.should.be.bignumber.equal(rates[3])
-  })
-  it('check round 4 rate', async function () {
-    await this.crowdsale.send(goals[3].plus(ether(0.01)))
-    let rate = await this.crowdsale.getCurrentRate()
-    rate.should.be.bignumber.equal(rates[4])
-  })
+    let token = await this.crowdsale.getToken().should.be.fulfilled
 
-  it('should accept payments during the sale', async function () {
     //ROUND 0
-    const expectedTokenAmount1 = rates[0].mul(ether(0.01));
-    await this.crowdsale.buyTokens(investor, { value: ether(0.01), from: investor }).should.be.fulfilled;
-    const balance = await this.crowdsale.balanceOf(investor);
-    balance.should.be.bignumber.equal(expectedTokenAmount1);
-
+    const expectedTokenAmount1 = rates[0].mul(ether(0.01))
+    await this.crowdsale.buyTokens(investor, { value: ether(0.01), from: investor }).should.be.fulfilled
+    const balance = await XdacToken.at(token).balanceOf(investor)
+    balance.should.be.bignumber.equal(expectedTokenAmount1)
 
     //ROUND 1
-    const expectedTokenAmount2 = rates[0].mul(ether(0.04)).add(rates[1].mul(ether(0.01))).add(expectedTokenAmount1);
-    await this.crowdsale.buyTokens(investor, { value: ether(0.05), from: investor }).should.be.fulfilled;
-    const balance2 = await this.crowdsale.balanceOf(investor);
-    balance2.should.be.bignumber.equal(expectedTokenAmount2);
-
+    const expectedTokenAmount2 = rates[0].mul(ether(0.04)).add(rates[1].mul(ether(0.01))).add(expectedTokenAmount1)
+    await this.crowdsale.buyTokens(investor, { value: ether(0.05), from: investor }).should.be.fulfilled
+    const balance2 = await XdacToken.at(token).balanceOf(investor)
+    balance2.should.be.bignumber.equal(expectedTokenAmount2)
 
     //ROUND 2
-    const expectedTokenAmount3 = rates[1].mul(ether(0.04)).add(rates[2].mul(ether(0.01))).add(expectedTokenAmount2);
-    await this.crowdsale.buyTokens(investor, { value: ether(0.05), from: investor }).should.be.fulfilled;
-    const balance3 = await this.crowdsale.balanceOf(investor);
-    balance3.should.be.bignumber.equal(expectedTokenAmount3);
+    const expectedTokenAmount3 = rates[1].mul(ether(0.04)).add(rates[2].mul(ether(0.01))).add(expectedTokenAmount2)
+    await this.crowdsale.buyTokens(investor, { value: ether(0.05), from: investor }).should.be.fulfilled
+    const balance3 = await XdacToken.at(token).balanceOf(investor)
+    balance3.should.be.bignumber.equal(expectedTokenAmount3)
 
     //ROUND 3
-    const expectedTokenAmount4 = rates[2].mul(ether(0.04)).add(rates[3].mul(ether(0.01))).add(expectedTokenAmount3);
-    await this.crowdsale.buyTokens(investor, { value: ether(0.05), from: investor }).should.be.fulfilled;
-    const balance4 = await this.crowdsale.balanceOf(investor);
-    balance4.should.be.bignumber.equal(expectedTokenAmount4);
+    const expectedTokenAmount4 = rates[2].mul(ether(0.04)).add(rates[3].mul(ether(0.01))).add(expectedTokenAmount3)
+    await this.crowdsale.buyTokens(investor, { value: ether(0.05), from: investor }).should.be.fulfilled
+    const balance4 = await XdacToken.at(token).balanceOf(investor)
+    balance4.should.be.bignumber.equal(expectedTokenAmount4)
 
     //ROUND 4
-    const expectedTokenAmount5 = rates[3].mul(ether(0.04)).add(rates[4].mul(ether(0.01))).add(expectedTokenAmount4);
-    await this.crowdsale.buyTokens(investor, { value: ether(0.05), from: investor }).should.be.fulfilled;
-    const balance5 = await this.crowdsale.balanceOf(investor);
-    balance5.should.be.bignumber.equal(expectedTokenAmount5);
+    const expectedTokenAmount5 = rates[3].mul(ether(0.04)).add(rates[4].mul(ether(0.01))).add(expectedTokenAmount4)
+    await this.crowdsale.buyTokens(investor, { value: ether(0.05), from: investor }).should.be.fulfilled
+    const balance5 = await XdacToken.at(token).balanceOf(investor)
+    balance5.should.be.bignumber.equal(expectedTokenAmount5)
 
     //ROUND 4
-    const expectedTokenAmount6 = rates[4].mul(ether(0.04)).add(expectedTokenAmount5);
-    await this.crowdsale.buyTokens(investor, { value: ether(0.04), from: investor }).should.be.fulfilled;
-    const balance6 = await this.crowdsale.balanceOf(investor);
-    balance6.should.be.bignumber.equal(expectedTokenAmount6);
+    const expectedTokenAmount6 = rates[4].mul(ether(0.04)).add(expectedTokenAmount5)
+    await this.crowdsale.buyTokens(investor, { value: ether(0.04), from: investor }).should.be.fulfilled
+    const balance6 = await XdacToken.at(token).balanceOf(investor)
+    balance6.should.be.bignumber.equal(expectedTokenAmount6)
 
-  });
+  })
 
-  it('should allow withdraw tokens after cap is reached', async function () {
-    await this.crowdsale.buyTokens(investor, { value: goals[4], from: investor }).should.be.fulfilled;
-    await this.crowdsale.withdrawTokens({ from: investor }).should.be.fulfilled;
-    let token = await this.crowdsale.getToken();
-    let balance = await XdacToken.at(token).balanceOf(investor)
-    let expectedTokenAmount = new BigNumber(0);
-    for (let round = 0; round < 5; round++) {
-      if(round === 0) {
-        expectedTokenAmount = expectedTokenAmount.add(goals[round].mul(rates[round]));
-      }
-      else {
-        expectedTokenAmount = expectedTokenAmount.add((goals[round].minus(goals[round-1])).mul(rates[round]));
-      }
-    }
+  it('should forward ether and tokens after whitelisting', async function () {
 
-    balance.should.be.bignumber.equal(expectedTokenAmount);
-  });
+    const token = await this.crowdsale.getToken().should.be.fulfilled
+
+    const expectedTokenAmount1 = rates[0].mul(ether(0.01))
+    await this.crowdsale.buyTokens(investor, { value: ether(0.01), from: investor }).should.be.fulfilled
+    const beforeWhitelisting = await this.crowdsale.getContributorValues(investor).should.be.fulfilled
+    beforeWhitelisting[0].should.be.bignumber.equal(ether(0.01))
+
+    await this.crowdsale.whitelistAddress(investor).should.be.fulfilled
+
+    const afterWhitelisting = await this.crowdsale.getContributorValues(investor).should.be.fulfilled
+    afterWhitelisting[1].should.be.true
+    const balance = await XdacToken.at(token).balanceOf(investor)
+    balance.should.be.bignumber.equal(expectedTokenAmount1)
+  })
+
+  //
+  // it('check round pre ICO rate', async function () {
+  //   await this.crowdsale.buyTokens(investor, { value: goals[0].minus(ether(0.01)), from: investor }).should.be.fulfilled;
+  //   let rate = await this.crowdsale.getCurrentRate()
+  //   rate.should.be.bignumber.equal(rates[0])
+  // })
+  //
+  // it('check round 1 rate', async function () {
+  //   await this.crowdsale.buyTokens(investor, { value: goals[0].plus(ether(0.01)), from: investor }).should.be.fulfilled;
+  //   let rate = await this.crowdsale.getCurrentRate()
+  //   rate.should.be.bignumber.equal(rates[1])
+  // })
+  // it('check round 2 rate', async function () {
+  //   await this.crowdsale.buyTokens(investor, { value: goals[1].plus(ether(0.01)), from: investor }).should.be.fulfilled;
+  //   let rate = await this.crowdsale.getCurrentRate()
+  //   rate.should.be.bignumber.equal(rates[2])
+  // })
+  // it('check round 3 rate', async function () {
+  //   await this.crowdsale.buyTokens(investor, { value: goals[2].plus(ether(0.01)), from: investor }).should.be.fulfilled;
+  //   let rate = await this.crowdsale.getCurrentRate()
+  //   rate.should.be.bignumber.equal(rates[3])
+  // })
+  // it('check round 4 rate', async function () {
+  //   await this.crowdsale.buyTokens(investor, { value: goals[3].plus(ether(0.01)), from: investor }).should.be.fulfilled;
+  //   let rate = await this.crowdsale.getCurrentRate()
+  //   rate.should.be.bignumber.equal(rates[4])
+  // })
+
+  // it('should allow withdraw tokens after cap is reached', async function () {
+  //   await this.crowdsale.buyTokens(investor, { value: goals[4], from: investor }).should.be.fulfilled;
+  //   await this.crowdsale.withdrawTokens({ from: investor }).should.be.fulfilled;
+  //   let token = await this.crowdsale.getToken();
+  //   let balance = await XdacToken.at(token).balanceOf(investor)
+  //   let expectedTokenAmount = new BigNumber(0);
+  //   for (let round = 0; round < 5; round++) {
+  //     if(round === 0) {
+  //       expectedTokenAmount = expectedTokenAmount.add(goals[round].mul(rates[round]));
+  //     }
+  //     else {
+  //       expectedTokenAmount = expectedTokenAmount.add((goals[round].minus(goals[round-1])).mul(rates[round]));
+  //     }
+  //   }
+  //
+  //   balance.should.be.bignumber.equal(expectedTokenAmount);
+  // });
 
 })
